e800370900204502439ce4e2e94df0449adc639e
victory-lydia <vickylydy@gmail.com>
2025-04-12 13:01:20 +0100

    flask

---
+++ flask_dev.py
@@ -0,0 +1,14 @@
+from flask import Flask
+app = Flask(__name__)
+
+def index():
+  return '<h1>SIU WELCOMES YOU!</h1>'
+app.add_url_rule('/', 'index', index)
+
+@app.route('/user/<name>')
+def user(name):
+ return '<h1>Welcome, {}!</h1>'.format(name)
+
+if __name__ == "__main__":
+    app.run(debug=True)
+    
