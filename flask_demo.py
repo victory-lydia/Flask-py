e4727ef56067e6f66dc21d818584718cd7e64c17
victory-lydia <vickylydy@gmail.com>
2025-04-12 12:36:41 +0100

    flask

--- flask_demo.py
+++ flask_demo.py
@@ -1,20 +1,3 @@
-# from flask import Flask
-
-# app = Flask(__name__)
-
-# @app.route('/hello')
-# def index():
-#     return "hello world"
-
-# def username(name):
-#     return f"username: {name}"
-
-# app.add_url_rule("/hello/<name>", view_func=username)
-
-# if __name__ == "__main__":
-#     app.run(debug=True)
-
-
 from flask import Flask
 app = Flask(__name__)
 
