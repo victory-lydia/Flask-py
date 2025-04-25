b8448ab1c720fcd35ae9a127893eaed8eb2954ae
victory-lydia <vickylydy@gmail.com>
2025-04-25 13:49:55 +0100

    flask

--- flask_dev.py
+++ flask_dev.py
@@ -12,3 +12,25 @@ def user(name):
 if __name__ == "__main__":
     app.run(debug=True)
     
+    with open('file.txt', 'r') as file:
+      content = file.read()
+      print(content)
+
+
+
+
+
+#       from flask import Flask
+# app = Flask(__name__)
+
+# def index():
+#   return '<h1>SIU WELCOMES YOU!</h1>'
+# app.add_url_rule('/', 'index', index)
+
+# @app.route('/user/<name>')
+# def user(name):
+#  return '<h1>Welcome, {}!</h1>'.format(name)
+
+# if __name__ == "__main__":
+#     app.run(debug=True)
+    
